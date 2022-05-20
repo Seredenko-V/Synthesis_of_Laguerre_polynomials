166c666aa362a3631ecd0a3c62a51b8339f7cf29
Seredenko-V <seredenko.spb@gmail.com>
2022-05-20 15:08:49 +0300

    Написал методы взаимодействия с классом

--- Synthesis_of_Laguerre_polynomials/Synthesis_of_Laguerre_polynomials.cpp
+++ Synthesis_of_Laguerre_polynomials/Synthesis_of_Laguerre_polynomials.cpp
@@ -1,9 +1,38 @@
 ﻿#include <iostream>
+#include <vector>
+#include <map>
 
 using namespace std;
 
+class Polynomial {
+public:
+    Polynomial() = default;
+
+    explicit Polynomial(const size_t quantity_polynomials) // сделать защиту от отрицательных чисел
+        : quantity_polynomials_(quantity_polynomials)
+    {
+        cout << "number_polynomials = " << quantity_polynomials << endl;
+    }
+
+    void SetQuantityPolynomials(const size_t quantity_polynomials) {
+        this->quantity_polynomials_ = quantity_polynomials;
+    }
+
+    vector<int32_t> GetPolynom(const size_t number_polynomial) {
+        return Laguerre_polynomials_.at(number_polynomial);
+    }
+
+    map<size_t, vector<int32_t>> GetAllPolynomials() {
+        return Laguerre_polynomials_;
+    }
+
+private:
+    size_t quantity_polynomials_ = 2;
+    map<size_t, vector<int32_t>> Laguerre_polynomials_ = { {0, {1}}, {1, {1 , -1}} };
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    Polynomial first(2);
     return 0;
 }
