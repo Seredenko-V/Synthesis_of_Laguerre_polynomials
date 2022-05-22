70b9d710e45cb4081f31592351a1d89bcc405b09
Seredenko-V <seredenko.spb@gmail.com>
2022-05-22 19:47:19 +0300

    Написал умножение полиномов через map

--- Synthesis_of_Laguerre_polynomials/Synthesis_of_Laguerre_polynomials.cpp
+++ Synthesis_of_Laguerre_polynomials/Synthesis_of_Laguerre_polynomials.cpp
@@ -7,37 +7,9 @@ using namespace std;
 //class Polynomial {
 //public:
 //    Polynomial() = default;
-//
-//    explicit Polynomial(const size_t quantity_polynomials) // сделать защиту от отрицательных чисел
-//        : quantity_polynomials_(quantity_polynomials)
-//    {
-//        cout << "number_polynomials = " << quantity_polynomials << endl;
-//    }
-//
-//    explicit Polynomial(const size_t quantity_polynomials, const double alpha)
-//        : quantity_polynomials_(quantity_polynomials)
-//        , alpha_(alpha)
-//    {
-//        cout << "number_polynomials = " << quantity_polynomials << endl;
-//        cout << "alpha_ = " << alpha_ << endl;
-//    }
-//
-//    void SetQuantityPolynomials(const size_t quantity_polynomials) {
-//        this->quantity_polynomials_ = quantity_polynomials;
-//    }
-//
-//    vector<int32_t> GetPolynom(const size_t number_polynomial) {
-//        return Laguerre_polynomials_.at(number_polynomial);
-//    }
-//
-//    map<size_t, vector<int32_t>> GetAllPolynomials() {
-//        return Laguerre_polynomials_;
-//    }
-//
+//    
 //private:
-//    double alpha_ = 0; // параметр масштаба
-//    size_t quantity_polynomials_ = 2;
-//    map<size_t, map<int32_t, double>> Laguerre_polynomials_ = { {0, {1}}, {1, {1 , -1}} };
+//    map<int32_t, double> polynomial_;
 //};
 
 void AssertImpl(bool value, const string& expr_str, const string& file, const string& func, unsigned line,
@@ -56,29 +28,62 @@ void AssertImpl(bool value, const string& expr_str, const string& file, const st
 #define ASSERT(expr) AssertImpl(!!(expr), #expr, __FILE__, __FUNCTION__, __LINE__, ""s)
 #define ASSERT_HINT(expr, hint) AssertImpl(!!(expr), #expr, __FILE__, __FUNCTION__, __LINE__, (hint))
 
-void PrintAllPolynomials(const vector<map<int32_t, double>>& all_polynom) {
-    for (const map<int32_t, double>& polynom : all_polynom) {
-        for (const auto& [exp, coef] : polynom) {
-            if (exp == 0) {
-                cout << coef;
-            } else {
-                if (coef > 0) {
-                    cout << "+"s << coef << "x^"s << exp;
-                } else {
-                    cout << coef << "x^"s << exp;
-                }
+void PrintPolynomial(const map<int32_t, double>& polynom) {
+    for (const auto& [exponent, coefficient] : polynom) {
+        if (exponent == 0) {
+            cout << coefficient;
+        } else {
+            if (coefficient > 0) {
+                cout << "+"s << coefficient << "x^"s << exponent;
+            }
+            else {
+                cout << coefficient << "x^"s << exponent;
             }
         }
+    }
+}
+
+void PrintAllPolynomials(const vector<map<int32_t, double>>& all_polynom) {
+    for (const map<int32_t, double>& polynom : all_polynom) {
+        PrintPolynomial(polynom);
         cout << endl;
     }
+}
+
+map<int32_t, double> operator*(const map<int32_t, double>& first, const map<int32_t, double>& second) {
+    map<int32_t, double> result;
+    for (const auto& [exponent_first, coefficient_first] : first) {
+        for (const auto& [exponent_second, coefficient_second] : second) {
+            result[exponent_first + exponent_second] += coefficient_first * coefficient_second;
+        }
+    }
+    PrintPolynomial(result);
+    return result;
+}
+
+template <typename Value> 
+map<int32_t, double> operator*(Value value, const map<int32_t, double>& polynomial) {
+    map<int32_t, double> result = polynomial;
+    for (const auto& [exponent, coefficient] : result) {
+        result[exponent] = coefficient * value;
+    }
+    PrintPolynomial(result);
+    return result;
+}
+
+map<int32_t, double> operator-(const map<int32_t, double>& first, const map<int32_t, double>& second) {
+    map<int32_t, double> result;
+
 }
 
 void SynthesisLaguerrePolynomials(vector<map<int32_t, double>>& all_polynom) {
     for (size_t i = 2; i < all_polynom.size(); ++i) {
         map<int32_t, double> tmp_polynom = { {0, 2 * i - 1}, {1, 1} };
-        // написать перегрузку оператора умножения для полиномов
-        all_polynom[i] = ((tmp_polynom * all_polynom[i - 1]) - ((i - 1) * all_polynom[i - 2])) / i;
+        //all_polynom[i] = ((tmp_polynom * all_polynom[i - 1]) - ((i - 1) * all_polynom[i - 2])) / i;
     }
+    map<int32_t, double> a = { {2,1}, {1,2}, {0,1} };
+    map<int32_t, double> b = { {3,1}, {1,2} };
+    map<int32_t, double> mult = a * b;
 }
 
 int main()
